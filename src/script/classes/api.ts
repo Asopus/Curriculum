3ff2fdefe14fd3fa73f0b68fa150cba5d32360a9
Pieter Vermeersch <vermeersch.pieter@live.be>
2020-10-19 23:46:26 +0200

    add mapper method

--- src/script/classes/api.ts
+++ src/script/classes/api.ts
@@ -5,30 +5,33 @@ export class Api {
 
     public async ReadCompetences(): Promise<Array<Competence>>
     {
+        let self = this;
         let _competences: Array<Competence> = new Array<Competence>();
-
         await $.ajax({
             url: "../../assets/competences/competences.json",
             type: "GET",
-            success: function (response) {
-                let result = response;
-                for(let i=0;i< result.length;++i)
-                {
-                    let currentCompetence = result[i];
-                    let technologies: Array<Technology> = new Array<Technology>();
-                    for(let j=0;j< currentCompetence.technologies.length;++j)
-                    {
-                        let currentTechnology = currentCompetence.technologies[j];
-                        technologies.push(new Technology(currentTechnology.title, currentTechnology.rating))
-                    }
-                    let competence = new Competence(currentCompetence.title, currentCompetence.imgPath, technologies);
-                    _competences.push(competence);
-                }
-                this._competences = _competences;
-            },
+            success: function (response) { _competences = self.MapCompetences(response); },
             error: function (xhr, status) { }
         });
 
+        return _competences;
+    }
+
+    private MapCompetences(response:any): Array<Competence>
+    {
+        let _competences: Array<Competence> = new Array<Competence>();
+        for(let i=0;i< response.length;++i)
+        {
+            let currentCompetence = response[i];
+            let technologies: Array<Technology> = new Array<Technology>();
+            for(let j=0;j< currentCompetence.technologies.length;++j)
+            {
+                let currentTechnology = currentCompetence.technologies[j];
+                technologies.push(new Technology(currentTechnology.title, currentTechnology.rating))
+            }
+            let competence = new Competence(currentCompetence.title, currentCompetence.imgPath, technologies);
+            _competences.push(competence);
+        }
         return _competences;
     }
 }
